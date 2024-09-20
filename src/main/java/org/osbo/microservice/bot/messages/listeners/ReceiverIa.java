128cc30555bd6b70a96116c1a1b8d8ff161d1c9e
Joaquín Heredia <joaquin.heredia@litermi.com>
2024-09-19 21:04:45 -0400

    Refactor ReceiverIa class to print response body before extracting values

--- src/main/java/org/osbo/microservice/bot/messages/listeners/ReceiverIa.java
+++ src/main/java/org/osbo/microservice/bot/messages/listeners/ReceiverIa.java
@@ -72,6 +72,8 @@ public class ReceiverIa {
                 String res = "";
                 String context = "";
                 if (!"true".equals(deb)) {
+                    System.out.println(response.getStatus());
+
                     System.out.println(response.getBody().getObject().toString());
                     res = response.getBody().getObject().getString("response");
                     JSONArray arra = response.getBody().getObject().getJSONArray("context");
